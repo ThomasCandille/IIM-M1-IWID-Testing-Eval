35953b6dbec7fbb4849edc7c79139c86001f77d2
Thomas Candille <thomascandille0508@gmail.com>
2025-10-17 15:54:52 +0200

    feat: ajout du premier test pour vérifier si la class produit contient bien le nom du produit

---
+++ src/index.test.ts
@@ -0,0 +1,12 @@
+import {describe, test, expect } from "@jest/globals";
+
+describe("Mon produit doit", () => {
+    test("Avoir un nom", () => {
+
+        const nomProduit = "Pomme";
+
+        const produit = new Produit(nomProduit);
+
+        expect(produit.nom).toBe(nomProduit);
+    });
+});
